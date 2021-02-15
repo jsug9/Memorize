a06967b76e39606a90ba9e0fe420f256c0665822
Augusto Galindo <jesus_augga_99@icloud.com>
2021-02-15 08:49:06 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Augusto Galindo Alí on 15/02/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
